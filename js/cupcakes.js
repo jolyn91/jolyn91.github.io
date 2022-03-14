c3ce27e64f62b26717d9550ff4eca8de7f690eee
Jolyn Foo <joyofoln@gmail.com>
2022-03-15 01:22:24 +0800

    Added ids to track event scrolls

--- js/cupcakes.js
+++ js/cupcakes.js
@@ -1,17 +1,19 @@
 let isScrolling = null;
 let newScrollSessionStarted = false;
+let id;
 
 window.addEventListener('scroll', (e) => {
     clearTimeout(isScrolling);
     isScrolling = setTimeout(() => {
-        logEventUtil('endBrowsingScroll');
+        logEventUtil('endBrowsingScroll', {id});
         newScrollSessionStarted = false;
     }, 150)
 }, false);
 
 const onScrollStart = () => {
     if (!newScrollSessionStarted) {
-        logEventUtil('startBrowsingScroll');
+        id = Math.floor(Math.random() * 100000000000);
+        logEventUtil('startBrowsingScroll', {id});
         newScrollSessionStarted = true;
     }
 };
