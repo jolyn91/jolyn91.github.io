67886ec91f32f06e56d7a5499d7463a6a7f4fac6
Jolyn Foo <joyofoln@gmail.com>
2022-03-16 22:14:51 +0800

    Update log event for scrolling

--- js/cupcakes.js
+++ js/cupcakes.js
@@ -1,19 +1,15 @@
 let isScrolling = null;
-let newScrollSessionStarted = false;
+let logNow = true;
 let id;
 
 window.addEventListener('scroll', (e) => {
-    clearTimeout(isScrolling);
-    isScrolling = setTimeout(() => {
-        logEventUtil('endBrowsingScroll', {id});
-        newScrollSessionStarted = false;
-    }, 150)
-}, false);
-
-const onScrollStart = () => {
-    if (!newScrollSessionStarted) {
-        id = Math.floor(Math.random() * 100000000000);
-        logEventUtil('startBrowsingScroll', {id});
-        newScrollSessionStarted = true;
+    if(logNow) {
+        logEventUtil('scrollStartEvent');
+        logNow = false
     }
-};
+    clearTimeout(isScrolling);
+    isScrolling = setTimeout(function () {
+        logNow = true;
+        logEventUtil('scrollStopEvent');
+    }, 500);
+}, false);
