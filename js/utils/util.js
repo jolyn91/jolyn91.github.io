dbe008fd35dfbc1153a1fa94f5c1480b54118222
gaurav-me <gsharma.nus@gmail.com>
2022-03-19 02:16:57 +0800

    Remove forms, homepage image links and add alerts

--- js/utils/util.js
+++ js/utils/util.js
@@ -1,21 +1,24 @@
 const logEventUtil = (eventName, additionalInfoObj) => {
-    let searchParams = {};
+  let searchParams = {};
 
-    if ('URLSearchParams' in window) {
-        searchParams = new URLSearchParams(
-            window.location.search.substring(1),
-        );
-    }
+  if ('URLSearchParams' in window) {
+    searchParams = new URLSearchParams(window.location.search.substring(1));
+  }
 
-    loggingjs.logEvent(null, eventName, {
-        eventName: eventName,
-        info: {
-            input: searchParams.get('input'),
-            unique_id: searchParams.get('unique_id'),
-            interface: 'old',
-            ...additionalInfoObj
-        },
-    })
-}
+  loggingjs.logEvent(null, eventName, {
+    eventName: eventName,
+    info: {
+      input: searchParams.get('input'),
+      unique_id: searchParams.get('unique_id'),
+      interface: 'old',
+      ...additionalInfoObj,
+    },
+  });
+};
 
-
+const onClickAddToCart = (eventName) => {
+  logEventUtil(eventName);
+  alert(
+    'Cupcake added to cart! This is the end of the flow, you may now close this tab.',
+  );
+};
